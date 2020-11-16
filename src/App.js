7fe684d18fa90da8bd0262b197bfa5dd6752271a
Gevorg Davtyan <gevorgdavtyan99@mail.ru>
2020-11-16 18:37:09 +0400

    vaxarshapt

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
           <div>
             <div className={style.footer}>
               <div className={style.container}>
-                  <div className={style.footer__text}>vardan</div>
+                  <div className={style.footer__text}>halidzor</div>
               </div>
             </div>
           </div>
