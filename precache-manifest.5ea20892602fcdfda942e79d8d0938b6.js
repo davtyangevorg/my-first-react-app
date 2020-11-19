d0169654b54b4d6d84881496c0e141003e9e4539
Gevorg Davtyan <gevorgdavtyan99@mail.ru>
2020-11-19 18:31:40 +0400

    Updates

---
+++ precache-manifest.5ea20892602fcdfda942e79d8d0938b6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97d1d8d31428b5aa9aed3c32a509712",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "c6a725ea25bcbb9c5484",
+    "url": "/my-first-react-app/static/css/main.6b955846.chunk.css"
+  },
+  {
+    "revision": "d8304ffd514640e77e89",
+    "url": "/my-first-react-app/static/js/2.fc4fa94e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-first-react-app/static/js/2.fc4fa94e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a725ea25bcbb9c5484",
+    "url": "/my-first-react-app/static/js/main.9519f02a.chunk.js"
+  },
+  {
+    "revision": "b7afb4358ac48f33d074",
+    "url": "/my-first-react-app/static/js/runtime-main.eb64b6ba.js"
+  },
+  {
+    "revision": "f8a58435f437c64b3463af5627270f0b",
+    "url": "/my-first-react-app/static/media/homepage.f8a58435.png"
+  },
+  {
+    "revision": "4fc700ed238d95d42912293d1331788d",
+    "url": "/my-first-react-app/static/media/loading.4fc700ed.svg"
+  },
+  {
+    "revision": "393eac12b76b592f8cce67ae3a53acae",
+    "url": "/my-first-react-app/static/media/logo.393eac12.png"
+  },
+  {
+    "revision": "d63d9eff35aa057bab9c5041938d6d69",
+    "url": "/my-first-react-app/static/media/product-1.d63d9eff.png"
+  },
+  {
+    "revision": "ad7ef891046b2a56aa41db25cbcf2a9b",
+    "url": "/my-first-react-app/static/media/product-2.ad7ef891.png"
+  },
+  {
+    "revision": "d27ec2bc66073aa4b0b80368a9ac410e",
+    "url": "/my-first-react-app/static/media/product-3.d27ec2bc.png"
+  },
+  {
+    "revision": "d9ed75372e686c2f8c38a53a52482e66",
+    "url": "/my-first-react-app/static/media/product-4.d9ed7537.png"
+  },
+  {
+    "revision": "8f9a1f89508713ff90dc1b497c92b1b2",
+    "url": "/my-first-react-app/static/media/product-5.8f9a1f89.png"
+  },
+  {
+    "revision": "b49d1b7cf1b82dad0a8059302706685a",
+    "url": "/my-first-react-app/static/media/product-6.b49d1b7c.png"
+  },
+  {
+    "revision": "4c6ab223c32bec22d9cd6070caaedf19",
+    "url": "/my-first-react-app/static/media/product-7.4c6ab223.png"
+  },
+  {
+    "revision": "d390282f89018d8777d36210b7df6d7b",
+    "url": "/my-first-react-app/static/media/product-8.d390282f.png"
+  }
+]);
